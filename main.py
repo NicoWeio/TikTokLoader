66f85ed4f9a8bebc1a65fca6a09df171ebbc702f
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2023-08-07 21:20:50 +0200

    wait for captcha solving

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from bs4 import BeautifulSoup
 from rich.console import Console
 from rich.progress import track
 from selenium import webdriver
+from time import sleep
 
 console = Console()
 
 
-def get_video_urls(driver, username):
-    driver.get(f"https://www.tiktok.com/@{username}")
+def get_video_urls(driver, username, reload_page=True):
+    if reload_page:  # can be false for CAPTCHA retries
+        driver.get(f"https://www.tiktok.com/@{username}")
 
     # TODO: check for HTTP 404
 
@@ -30,12 +32,24 @@ def get_video_urls(driver, username):
     return video_links
 
 
+def get_video_urls_retry(driver, username):
+    video_urls = get_video_urls(driver, username)
+    if not video_urls:
+        console.print(f"No videos found for {username}. Maybe you need to solve a CAPTCHA?", style="red")
+    while not video_urls:
+        with console.status(f"Monitoring page for changes…", spinner="dots"):
+            video_urls = get_video_urls(driver, username, reload_page=False)
+            sleep(1)  # can be fast, because this causes no interaction with the site
+
+    return video_urls
+
+
 @click.command()
 @click.argument("usernames", nargs=-1)
 def download_user(usernames):
     with webdriver.Firefox() as driver:
         for username in usernames:
-            video_urls = get_video_urls(driver, username)
+            video_urls = get_video_urls_retry(driver, username)
             print(f"Found {len(video_urls)} videos for {username}")
 
             user_subdir = Path(username)
